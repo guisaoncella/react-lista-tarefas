8288827aaa281dfb1633793bd5592545bf9da18b
Guilherme Saoncella da Silva <guisaoncella@gmail.com>
2022-09-21 13:20:22 -0300

    :lipstick: feat: create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import {Container} from './App.styles'
+import {Area, Container, Header} from './App.styles'
 
 const App = () => {
     return(
         <Container>
-            Hello
+            <Header>Lista de Tarefas</Header>
+            <Area>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro magni ex perspiciatis numquam ea nulla temporibus dolorum quidem alias illo necessitatibus illum ipsa facilis aspernatur, aliquid perferendis eos sunt animi.</Area>
         </Container>
     );
 }

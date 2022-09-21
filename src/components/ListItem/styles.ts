f27fce4541c0a088900e64aa22ddddb600dce00a
Guilherme Saoncella da Silva <guisaoncella@gmail.com>
2022-09-21 14:07:04 -0300

    :lipstick: feat: update ListItem style

--- src/components/ListItem/styles.ts
+++ src/components/ListItem/styles.ts
@@ -1,3 +1,22 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    display: flex;
+    background-color: #20212C;
+    padding: 10px;
+    border-radius: 10px;
+    margin-bottom: 10px;
+    align-items: center;
+
+    input{
+        width: 20px;
+        height: 20px;
+        margin-right: 10px;
+    }
+
+    label{
+        font-size: 18px;
+        font-weight: 600;
+        color: #ccc;
+    }
+`;
